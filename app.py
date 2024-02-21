35feb46497faed1b99a01be534b6a596cbb8f230
Grace Gallivan <gracee.gallivan@gmail.com>
2024-02-21 15:55:55 -0800

    add function

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 from flask import Flask, request
+from operations import add, sub, mult, div
 
 app =Flask(__name__)
 
 
+@app.get("/add")
+def do_add():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    print("type", type(result))
+    return str(result)
+
+
+# @app.get("/sub")
+# def sub():
+
+# @app.get("/mult")
+# def mult():
+
+# @app.get("/div")
+# def div():
